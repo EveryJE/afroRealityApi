f2a90e889ff4dd6b753f3da40714714de8fb0789
EveryJE <kgyan19lf@gmail.com>
2026-07-31 10:05:53 +0000

    init:project

---
+++ src/main/java/com/example/afrorealityapi/AfroRealityApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.afrorealityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfroRealityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfroRealityApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/afrorealityapi/AfroRealityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.afrorealityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfroRealityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
